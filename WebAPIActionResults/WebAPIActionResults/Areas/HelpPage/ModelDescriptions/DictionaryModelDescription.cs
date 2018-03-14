5857cdc1a8951bac2420ed9fda78df13842ac0f5
Kuldeep <ksnauhwar@gmail.com>
2018-03-14 21:04:06 +0530

    Practise Web api return types

---
+++ WebAPIActionResults/WebAPIActionResults/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPIActionResults.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebAPIActionResults/WebAPIActionResults/Controllers/HttpResponseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIActionResults.Controllers
+{
+    public class HttpResponseController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public HttpResponseMessage Get(int id)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK, "value");
+            response.Content =new StringContent( string.Format("{0}{1}", response.Content.ToString(), "honor"));
+            return response;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
